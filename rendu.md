b972dca37c856a912cfc1159cecb59ba3a2a684e
Benjamin Chancerel <benjamin.chancerel@effiliation.com>
2021-12-15 17:24:33 +0100

    First version of part 3

--- rendu.md
+++ rendu.md
@@ -28,3 +28,19 @@ on rentre alors dans la fonction piocher :
   - Ensuite si la condition est vérifié, elle va enregister dans la list une valeur entre 1 et 52 qui sera votre nouvelle carte.
   - Puis elle va incrémenter le compteur.
   - Enfin elle vous affiche la liste de vos cartes et le nombre de cartes que vous avez
+
+## III. Game
+
+**Voir commit numéro 3 (game) sur le git**
+
+J'ai d'abord fais le choix pour cette partie de tout regroupé dans un seul fichier (peut etre pas ma meilleure idée). Seulement mtn je manque de temps pour pouvoir refaire tout en divisant mon code.
+
+J'ai donc créer une classe game qui comprend les focntions **__init__**, **play**, **pioche** et **distribution** des cartes.
+
+Dans la fonction **init** j'ai donc écris les attributs de la classe c'est à dire les liste de cartes de chaques joueur.
+
+Dans la fonction **Play** on retrouve d'abord un appel aux focntions de distributions qui donneront lescartes de départs aux joueurs.
+Puis la focntion, demandera avec un input ce que vous voulez faire : piocher ou passez votre tour.
+
+Si vous piochez elle fera appel à la fonction pioche qui donnera des cartes aléatoires aux joueurs.
+
--- tp_Benjam.py
+++ tp_Benjam.py
@@ -14,7 +14,7 @@ class Card:
             + ", Couleur de la carte : " + self.color)
     
 card = Card("4", "red")
-print(card.Display())
+#print(card.Display())
 
 # II Cards
 
@@ -38,5 +38,102 @@ class Cards:
             print("bah dégage alors")
 
 cards = Cards()
+#while True:
+#    cards.piocher()
+
+# III. Game
+
+class Game:
+
+    def __init__(self):
+        self.listcardJoueur1    = []
+        self.listcardJoueur2    = []
+        self.listcardCroupier   = []
+
+        self.player = "J1"
+
+        Game.distribution_j(self)
+        Game.distribution_c(self)
+        Game.distribution_j(self)
+
+    def distribution_j(self):
+        new_card_1 = random.randint(1, 11)
+        new_card_2 = random.randint(1, 11)
+        if self.player == "J1":
+            self.listcardJoueur1.append(new_card_1)
+            self.listcardJoueur1.append(new_card_2)
+            print("Voici les nouvelles cartes du joueur 1 " \
+                + str(new_card_1) + " " \
+                + str(new_card_2))
+            self.player = "C"
+        elif self.player == "J2":
+            self.listcardJoueur2.append(new_card_1)
+            self.listcardJoueur2.append(new_card_2)
+            print("Voici les nouvelles cartes du joueur 2 " \
+                + str(new_card_1) + " "\
+                + str(new_card_2))
+            self.player = "J1"
+        
+    def distribution_c(self):
+        new_card = random.randint(1, 11)
+        if self.player == "C":
+            self.listcardCroupier.append(new_card)
+            print("Voici les nouvelles cartes du Croupier " \
+                + str(new_card))
+            self.player = "J2"
+
+
+    def pioche(self):
+        new_card    = 0
+
+        new_card = random.randint(1, 11)
+        if self.player == "J1":
+            self.listcardJoueur1.append(new_card)
+            print(new_card)
+            if sum(self.listcardJoueur1) > 21:
+                self.listcardJoueur1.clear()
+                print("dégage")
+        elif self.player == "J2":
+            self.listcardJoueur2.append(new_card)
+            print(new_card)
+            if sum(self.listcardJoueur1) > 21:
+                self.listcardJoueur1.clear()
+                print("dégage")
+        elif self.player == "C":
+            self.listcardCroupier.append(new_card)
+            print(new_card)
+            if sum(self.listcardJoueur1) > 21:
+                self.listcardJoueur1.clear()
+                print("dégage")
+    
+    def play(self):
+
+        print(self.player)
+        instruction = input("Que voulez vous faire ? |piocher|stop|")
+
+        if self.player == "J1":
+            print(self.listcardJoueur1)
+            while sum(self.listcardJoueur1) <= 21:
+                if instruction == "piocher" :
+                    Game.pioche(self)
+                    print(self.listcardJoueur1)
+                elif instruction == "stop":
+                    self.player = "J2"
+                    break
+                if instruction == "somme":
+                    print(str(sum(self.listcardJoueur1)))
+                elif sum(self.listcardJoueur1) == 21:
+                    print("You win !")
+                    self.player = "J2"
+                    break
+                elif sum(self.listcardJoueur1) > 21:
+                    print("You're out")
+                    self.player = "J2"
+                    self.listcardJoueur1.clear()
+                    break   
+
+game = Game()
 while True:
-    cards.piocher()
+    game.play()
+
+
